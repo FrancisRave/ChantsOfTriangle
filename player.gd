bddf1d31d5ba0113ab25eb808c69f6630694bfd4
RaphaelNestor <nestor.martin.raphael@gmail.com>
2024-01-28 11:23:27 +0100

    suite parler

--- player.gd
+++ player.gd
@@ -3,30 +3,37 @@ extends CharacterBody3D
 @export var speed = 5
 
 var target_velocity = Vector3.ZERO
+var parle = false
+
 
 func _physics_process(delta):
-	# We create a local variable to store the input direction.
+	
+	# mouvement 
+	
 	var direction = Vector3.ZERO
 
-	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
 	if Input.is_action_pressed("move_down"):
-		# Notice how we are working with the vector's x and z axes.
-		# In 3D, the XZ plane is the ground plane.
 		direction.z += 1
 	if Input.is_action_pressed("move_up"):
 		direction.z -= 1
-		
 	if (direction != Vector3.ZERO):
 		direction = direction.normalized()
 		$Pivot.basis = Basis.looking_at(direction)
-	
 	target_velocity.z = direction.z * speed
 	target_velocity.x = direction.x * speed
-	
-		
 	velocity = target_velocity
 	move_and_slide()
+	
+	# parler
+	
+	if Input.is_action_just_pressed("parler"):
+		if (parle):
+			$parler.visible = false
+			parle = false
+		else:
+			$parler.visible = true
+			parle = true
